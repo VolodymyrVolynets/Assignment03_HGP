5ba0b658567140cab6c9af11253841813b9c08fa
Ivan Zhukov <ivan.zhukov@student.griffith.ie>
2022-12-24 04:47:56 +0000

    fix Main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QApplication
 from go import Go
 import sys
-
-app = QApplication([])
-myGo = Go()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QApplication([])
+    myGo = Go()
+    sys.exit(app.exec_())
