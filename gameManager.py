824f02c66be721d66fd46463bbe05d267ee863e9
Albert Kison <albert.kison@student.griffith.ie>
2022-12-24 11:52:55 +0000

    Fix removing stones

--- gameManager.py
+++ gameManager.py
@@ -20,17 +20,24 @@ class GameManager():
         self.board_array = np.zeros((board_size, board_size))
 
         self.black_score = 0
+        self.black_player_stones_eaten = 0
         self.white_score = 0
+        self.white_player_stones_eaten = 0
 
     def addUpdateUICallback(self, updateUiMethod):
         updateUiMethod()
 
     def cellPressed(self, x, y):
 
-        # check if the stone is already placed
+        # check if the move is valid
         if self.board_array[y][x] == 0:
             self.board_array[y][x] = 1 if self.white_turn else 2
+            if self.getLiberties(y, x) == 0:
+                self.board_array[y][x] = 0
+                print("Invalid move")
+                return
         else:
+            print("Move not valid")
             return
 
         print(self.board_array)
@@ -65,10 +72,19 @@ class GameManager():
         #         liberties[i][j] = 0
 
         print(liberties)
+        white_player_stones_eaten = 0
+        black_player_stones_eaten = 0
         for i in range(0, self.board_size):
             for j in range(0, self.board_size):
                 if liberties[i][j] == 0:
+                    if not self.board_array[i][j] == 0:
+                        if self.white_turn:
+                            white_player_stones_eaten += 1
+                        else:
+                            black_player_stones_eaten += 1
                     self.board_array[i][j] = 0
+        print(white_player_stones_eaten)
+        print(black_player_stones_eaten)
 
         # update score
         for i in range(0, self.board_size):
@@ -78,6 +94,14 @@ class GameManager():
                         self.white_score += 1
                     else:
                         self.black_score += 1
+                if not liberties[i][j] == self.prev_liberties[i][j] and self.prev_liberties[i][j] == 0:
+                    if self.white_turn:
+                        self.black_score -= 1
+                    else:
+                        self.white_score -= 1
+
+        self.white_score += white_player_stones_eaten
+        self.black_score += black_player_stones_eaten
 
         print(f"Black player score: {self.black_score}")
         print(f"White player score: {self.white_score}")
@@ -90,35 +114,39 @@ class GameManager():
         temp = self.board_array[i][j]
         if not j - 1 < 0:
             # if not self.board_array[i][j] == 0:
-                if self.board_array[i][j - 1] == 0:
-                    count += 1
-                elif self.board_array[i][j - 1] == self.board_array[i][j]:
-                    self.board_array[i][j] = 100
-                    count += self.getLiberties(i, j-1)
+            if self.board_array[i][j - 1] == 0:
+                count += 1
+            elif self.board_array[i][j - 1] == self.board_array[i][j]:
+                self.board_array[i][j] = 100
+                count += self.getLiberties(i, j-1)
+                self.board_array[i][j] = temp
         if not j + 1 >= self.board_size:
             # if not self.board_array[i][j] == 0:
-                if self.board_array[i][j + 1] == 0:
-                    count += 1
-                elif self.board_array[i][j + 1] == self.board_array[i][j]:
-                    self.board_array[i][j] = 101
-                    count += self.getLiberties(i, j+1)
+            if self.board_array[i][j + 1] == 0:
+                count += 1
+            elif self.board_array[i][j + 1] == self.board_array[i][j]:
+                self.board_array[i][j] = 101
+                count += self.getLiberties(i, j+1)
+                self.board_array[i][j] = temp
         if not i - 1 < 0:
             # if not self.board_array[i][j] == 0:
-                if self.board_array[i - 1][j] == 0:
-                    count += 1
-                elif self.board_array[i - 1][j] == self.board_array[i][j]:
-                    self.board_array[i][j] = 102
-                    count += self.getLiberties(i-1, j)
+            if self.board_array[i - 1][j] == 0:
+                count += 1
+            elif self.board_array[i - 1][j] == self.board_array[i][j]:
+                self.board_array[i][j] = 102
+                count += self.getLiberties(i-1, j)
+                self.board_array[i][j] = temp
         if not i + 1 >= self.board_size:
             # if not self.board_array[i][j] == 0:
-                if self.board_array[i + 1][j] == 0:
-                    count += 1
-                elif self.board_array[i + 1][j] == self.board_array[i][j]:
-                    self.board_array[i][j] = 103
-                    count += self.getLiberties(i+1, j)
+            if self.board_array[i + 1][j] == 0:
+                count += 1
+            elif self.board_array[i + 1][j] == self.board_array[i][j]:
+                self.board_array[i][j] = 103
+                count += self.getLiberties(i+1, j)
+                self.board_array[i][j] = temp
 
-        self.board_array[i][j] = temp
         return count
+
 
     def passTurn(self):
         self.white_turn = not self.white_turn
