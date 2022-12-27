61f25a4b5028235d48a3c4ad0a0bc294ee14a9a4
Albert Kison <albert.kison@student.griffith.ie>
2022-12-27 11:35:26 +0000

    Fix layout

--- dockWidget.py
+++ dockWidget.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QGridLayout,QDialog, QWidget, QMainWindow, QFileDialog, QDockWidget, QPushButton, QLabel, QMessageBox
+from PyQt5.QtWidgets import QGridLayout,QDialog, QWidget, QMainWindow, QFileDialog, QDockWidget, QPushButton, QLabel, QMessageBox, QVBoxLayout, QHBoxLayout
 from PyQt5.QtGui import QOpenGLTimerQuery, QPainter, QPen, QPixmap
 from PyQt5.QtGui import QPalette, QPixmap,QBrush
 from PyQt5.QtCore import QTimer
@@ -18,10 +18,11 @@ class DockWidget(QDockWidget):
         self.timer.setInterval(1000)  # Set the interval to 1 second
         self.timer.timeout.connect(self.on_timeout)
         self.playerInfo.setStyleSheet("QWidget{background-color:#314532;}")
-        self.layout = QGridLayout()
+        self.layout = QVBoxLayout()
         self.black_score= self.game_manager.black_score
         self.turnLabel = QLabel("")
         self.blackScoreLabel = QLabel("")
+        self.turn_label_text = QLabel("Turn:")
         self.bolTurn = self.game_manager.white_turn
         self.skipButton = QPushButton("Skip")
         self.skipButton.clicked.connect(self.updateSkipTurn)
@@ -29,22 +30,130 @@ class DockWidget(QDockWidget):
         self.rulesButton.clicked.connect(self.goRulesDialog)
         self.skipButton.setStyleSheet("background-color:#6B8F45;")
         self.rulesButton.setStyleSheet("background-color:#6B8F45;")
-        self.blackScoreText = QLabel("Black Score:")
-        self.whiteScoreText = QLabel("White Score:")
+        self.blackScoreText = QLabel("Score:")
+        self.whiteScoreText = QLabel("Score:")
         self.whiteScoreLabel = QLabel("")
-        self.blackStonesEatenText = QLabel("White Stones Eaten:")
-        self.whiteStonesEatenText = QLabel("Black Stones Eaten")
+        self.blackStonesEatenText = QLabel("Stones:")
+        self.whiteStonesEatenText = QLabel("Stones:")
         self.blackStonesEatenLabel = QLabel("")
         self.whiteStonesEatenLabel = QLabel("")
         self.error_message_label = QLabel("")
-        self.whiteTerritoryText =QLabel("White Territory:")
-        self.blackTerritoryText = QLabel("Black Territory:")
+        self.error_message_label.setStyleSheet("color: red")
+        self.whiteTerritoryText =QLabel("Territory:")
+        self.blackTerritoryText = QLabel("Territory:")
         self.whiteTerritoryLabel = QLabel("")
         self.blackTerritoryLabel = QLabel("")
         self.restartButton = QPushButton("Restart")
         self.restartButton.clicked.connect(self.restartGame)
         self.restartButton.setStyleSheet("background-color:#6B8F45;")
         self.winLabel = QLabel("")
+
+        turns_row = QHBoxLayout()
+        turns_row.addWidget(self.turn_label_text)
+        turns_row.addStretch(1)
+        turns_row.addWidget(self.turnLabel)
+
+        black_player_row = QHBoxLayout()
+        black_player_row.addWidget(QLabel("Black player:"))
+
+        black_score_row = QHBoxLayout()
+        black_score_row.addWidget(self.blackScoreText)
+        black_score_row.addStretch(1)
+        black_score_row.addWidget(self.blackScoreLabel)
+
+        white_player_row = QHBoxLayout()
+        white_player_row.addWidget(QLabel("White player:"))
+
+        white_score_row = QHBoxLayout()
+        white_score_row.addWidget(self.whiteScoreText)
+        white_score_row.addStretch(1)
+        white_score_row.addWidget(self.whiteScoreLabel)
+
+        black_stones_eaten_row = QHBoxLayout()
+        black_stones_eaten_row.addWidget(self.blackStonesEatenText)
+        black_stones_eaten_row.addStretch(1)
+        black_stones_eaten_row.addWidget(self.blackStonesEatenLabel)
+
+        white_stones_eaten = QHBoxLayout()
+        white_stones_eaten.addWidget(self.whiteStonesEatenText)
+        white_stones_eaten.addStretch(1)
+        white_stones_eaten.addWidget(self.whiteStonesEatenLabel)
+
+        black_territory_row = QHBoxLayout()
+        black_territory_row.addWidget(self.blackTerritoryText)
+        black_territory_row.addStretch(1)
+        black_territory_row.addWidget(self.blackTerritoryLabel)
+
+        white_territory_row = QHBoxLayout()
+        white_territory_row.addWidget(self.whiteTerritoryText)
+        white_territory_row.addStretch(1)
+        white_territory_row.addWidget(self.whiteTerritoryLabel)
+
+        win_row = QHBoxLayout()
+        win_row.addWidget(self.winLabel)
+
+        skip_button_row = QHBoxLayout()
+        skip_button_row.addWidget(self.skipButton)
+
+        error_row = QHBoxLayout()
+        error_row.addWidget(self.error_message_label)
+
+        restart_button_row = QHBoxLayout()
+        restart_button_row.addWidget(self.restartButton)
+
+        rules_button_row = QHBoxLayout()
+        rules_button_row.addWidget(self.rulesButton)
+
+        # self.layout.addWidget(QLabel("Turn:"), 1, 1)
+        # self.layout.addWidget(self.turnLabel, 1, 2)
+        # self.layout.addWidget(self.blackScoreText, 2, 1)
+        # self.layout.addWidget(self.blackScoreLabel,2,2)
+        # self.layout.addWidget(self.whiteScoreText,3,1)
+        # self.layout.addWidget(self.whiteScoreLabel,3,2)
+        # self.layout.addWidget(self.blackStonesEatenText,4,1)
+        # self.layout.addWidget(self.blackStonesEatenLabel,5,1)
+        # self.layout.addWidget(self.blackTerritoryText,6,1)
+        # self.layout.addWidget(self.blackTerritoryLabel, 7, 1)
+        # self.layout.addWidget(self.whiteStonesEatenText,10,1)
+        # self.layout.addWidget(self.whiteStonesEatenLabel,11,1)
+        # self.layout.addWidget(self.whiteTerritoryText,12,1 )
+        # self.layout.addWidget(self.whiteTerritoryLabel,13,1 )
+        # self.layout.addWidget(self.winLabel, 14, 1)
+        # self.layout.addWidget(self.skipButton,14,1)
+        # self.layout.addWidget(QLabel(""), 15, 1)
+        # self.layout.addWidget(self.error_message_label,16,1)
+        # self.layout.addWidget(QLabel(""), 17, 1)
+        # self.layout.addWidget(self.restartButton, 18, 1)
+        # self.layout.addWidget(QLabel(""), 19, 1)
+        # self.layout.addWidget(self.rulesButton,20,1)
+        # self.layout.addWidget(self.timer_display,21,1)
+
+        self.layout.addLayout(turns_row)
+        self.layout.addStretch(3)
+        self.layout.addLayout(black_player_row)
+        self.layout.addLayout(black_score_row)
+        # self.layout.addStretch(1)
+        self.layout.addLayout(black_stones_eaten_row)
+        # self.layout.addStretch(1)
+        self.layout.addLayout(black_territory_row)
+        self.layout.addStretch(3)
+
+        self.layout.addLayout(white_player_row)
+        self.layout.addLayout(white_score_row)
+        # self.layout.addStretch(3)
+        self.layout.addLayout(white_stones_eaten)
+        # self.layout.addStretch(3)
+        self.layout.addLayout(white_territory_row)
+        # self.layout.addStretch(3)
+        self.layout.addLayout(win_row)
+
+        self.layout.addStretch(30)
+        self.layout.addLayout(error_row)
+        self.layout.addLayout(skip_button_row)
+        self.layout.addLayout(restart_button_row)
+        self.layout.addLayout(rules_button_row)
+
+        self.playerInfo.setLayout(self.layout)
         self.initUI()
 
 
@@ -53,39 +162,20 @@ class DockWidget(QDockWidget):
         # self.palette = QPalette()
         # self.palette.setBrush(QPalette.Background, QBrush(QPixmap('woodTable.jpg').scaled(self.size())))
         # self.setPalette(self.palette)
-        self.counter = 10
-        self.timer_display = QLabel(str(self.counter))
+        self.counter = 120
+        self.minutes = str(self.counter // 60)
+        self.seconds = "0" + str(self.counter % 60) if self.counter % 60 < 10 else str(self.counter % 60)
+        self.timer_display = QLabel(self.minutes + ":" + self.seconds)
         self.skipButton.show()
         self.winLabel.close()
-        self.resize(200, 900)
-        self.setFixedWidth(200)
+        self.resize(250, 900)
+        self.setFixedWidth(250)
         # self.setFixedSize(200,900)
-        self.layout.setSpacing(10)
+        # self.layout.setSpacing(10)
         print("i" + str(self.black_score))
-        self.setWindowTitle('ScoreBoard')
-        self.layout.addWidget(self.turnLabel, 1, 1)
-        self.layout.addWidget(self.blackScoreText, 2, 1)
-        self.layout.addWidget(self.blackScoreLabel,3,1)
-        self.layout.addWidget(self.blackStonesEatenText,4,1)
-        self.layout.addWidget(self.blackStonesEatenLabel,5,1)
-        self.layout.addWidget(self.blackTerritoryText,6,1)
-        self.layout.addWidget(self.blackTerritoryLabel, 7, 1)
-        self.layout.addWidget(self.whiteScoreText,8,1)
-        self.layout.addWidget(self.whiteScoreLabel,9,1)
-        self.layout.addWidget(self.whiteStonesEatenText,10,1)
-        self.layout.addWidget(self.whiteStonesEatenLabel,11,1)
-        self.layout.addWidget(self.whiteTerritoryText,12,1 )
-        self.layout.addWidget(self.whiteTerritoryLabel,13,1 )
-        self.layout.addWidget(self.winLabel, 14, 1)
-        self.layout.addWidget(self.skipButton,14,1)
-        self.layout.addWidget(QLabel(""), 15, 1)
-        self.layout.addWidget(self.error_message_label,16,1)
-        self.layout.addWidget(QLabel(""), 17, 1)
-        self.layout.addWidget(self.restartButton, 18, 1)
-        self.layout.addWidget(QLabel(""), 19, 1)
-        self.layout.addWidget(self.rulesButton,20,1)
-        self.layout.addWidget(self.timer_display,21,1)
-        self.playerInfo.setLayout(self.layout)
+        # self.setWindowTitle('ScoreBoard')
+
+
         self.setWidget(self.playerInfo)
         self.updateErrorMessage()
         if(self.error_message_value==""):
@@ -109,7 +199,9 @@ class DockWidget(QDockWidget):
         self.counter -= 1
 
         # Update the timer display
-        self.timer_display.setText(str(self.counter))
+        self.minutes = str(self.counter // 60)
+        self.seconds = "0" + str(self.counter % 60) if self.counter % 60 < 10 else str(self.counter % 60)
+        self.timer_display.setText(self.minutes + ":" + self.seconds)
 
         # If the counter reaches 0, stop the timer and take the appropriate action
         if self.counter == 0:
@@ -123,15 +215,13 @@ class DockWidget(QDockWidget):
 
     def updateTurn(self):
         self.bolTurn = self.game_manager.white_turn
-        print(self.bolTurn)
         if self.bolTurn:
-            self.turnLabel.setText("WHITE PLAYER TURN")
+            self.turnLabel.setText("White player")
 
         else:
-            self.turnLabel.setText("BLACK PLAYER TURN")
+            self.turnLabel.setText("Black player")
 
     def updateBlackScore(self):
-        print("PISOS")
         self.black_score = self.game_manager.black_score
         self.blackScoreLabel.setText(str(self.black_score))
 
@@ -147,7 +237,6 @@ class DockWidget(QDockWidget):
             elif (self.game_manager.black_score < self.game_manager.white_score):
                 self.winLabel.setText("WHITE WINS")
             self.winLabel.show()
-            print("2 times skiped")
         # QApplication.processEvents()
     def updateWhiteScore(self):
         self.whiteScoreLabel.setText(str(self.game_manager.white_score))
@@ -155,7 +244,6 @@ class DockWidget(QDockWidget):
         # self.game_manager.addUpdateUICallback(self)
     def updateWhiteStones(self):
         self.whiteEaten = self.game_manager.white_player_stones_eaten
-        print("white stones"+str(self.whiteEaten))
         self.whiteStonesEatenLabel.setText(str(self.whiteEaten))
     def updateBlackStones(self):
         self.blackEaten = self.game_manager.black_player_stones_eaten
@@ -163,7 +251,6 @@ class DockWidget(QDockWidget):
     def updateErrorMessage(self):
         self.error_message_value = self.game_manager.error_message
         self.timer.stop()
-        print("error"+str(self.error_message_value))
         self.error_message_label.setText(str(self.error_message_value))
     def updateBlackTerritory(self):
         self.blackTerritoryLabel.setText(str(self.game_manager.territory_controlled_by_black))
